f9e10ebdb9ec9025c21ca7a8981e50071026eac0
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2022-06-12 17:25:19 -0300

    DosPajarosVolandoProyectoFinalv2

--- formulario.php
+++ formulario.php
@@ -1,15 +1,15 @@
 <?php
     if(isset($_POST['enviar'])){
-        if(!empty($_POST['nombre'])&& !empty($_POST['email'])&&!empty($_POST['msg'])){
+        if(!empty($_POST['nombre'])&& !empty($_POST['email'])&&!empty($_POST['mensaje'])){
             $nombre=$_POST['nombre'];
             $email=$_POST['email'];
             $asunto="Consulta vía web"; 
-            $msg="Nombre: ".$nombre."\n".$_POST['msg'];
+            $mensaje="Nombre: ".$nombre."\n".$_POST['mensaje'];
             $header="From: ".$email."\r\n";
             $header.="Reply-To: contacto@dospajarosvolando.com"."\r\n";
             $header.="X-Mailer: PHP/".phpversion();
             $tuCasilla="contacto@dospajarosvolando.com";
-            $mail=mail($tuCasilla,$asunto,$msg,$header);
+            $mail=@mail($tuCasilla,$asunto,$mensaje,$header);
             if($mail){
                 echo "<script>
                         alert('Gracias por tu contacto! nos estaremos comunicando');
@@ -26,7 +26,7 @@
             echo "<script>
             alert('Error, faltan datos');
                     window.location='index.html'
-                  </script>"; 
+                </script>"; 
         }
     } 
      header("Location: formulario.php")
